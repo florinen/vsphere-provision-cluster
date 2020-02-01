9ed81125b6ff0487b753e1c77f5dcbc1136077f2
florinen <nenfl24@gmail.com>
2020-01-31 20:59:15 -0600

    added  option to remove ssh keys when destroy

--- ssh-key.tf
+++ ssh-key.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "generate-sshkey" {
   provisioner "local-exec" {
-    command = "yes y | ssh-keygen -b 4096 -t rsa -C 'prod-env' -N '' -f ${var.virtual_machine_kubernetes_controller["private_key"]}"
+    command = "yes y | ssh-keygen -b 4096 -t rsa -C '${var.env_name}' -N '' -f ${var.virtual_machine_kubernetes_controller["private_key"]}"
   }
 }
 
@@ -12,5 +12,11 @@ resource "null_resource" "ssh-keygen-delete" {
 resource "null_resource" "ssh-keygen-delete-nodes" {
   provisioner "local-exec" {
     command = "ssh-keygen -R ${cidrhost(var.virtual_machine_kubernetes_node["ip_address_network"], var.virtual_machine_kubernetes_node["starting_hostnum"])}"
+  }
+}
+resource "null_resource" "remove_ssh_keys" {
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "rm $HOME/.ssh/id_rsa-${var.env_name}*"
   }
 }
