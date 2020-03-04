480dec2dcf01972c46364bb98e70ab503dc0504a
Florin <nenfl24@gmail.com>
2020-03-03 20:01:01 -0600

    changed zsh with bash profile

--- scripts/setup_cluster_access.sh
+++ scripts/setup_cluster_access.sh
@@ -3,9 +3,9 @@ sed -i "/clusters/ {n;n;n;n;s/name: .*/name: ${env_name}/g}" $HOME/.kube/admin.c
 sed -i "/contexts/ {n;n;s/cluster: .*/cluster: ${env_name}/g}" $HOME/.kube/admin.conf
 sed -i "/contexts/ {n;n;n;n;s/name: .*/name: kubernetes-admin@${env_name}/g}" $HOME/.kube/admin.conf
 sed -i "s/current-context: .*/current-context: kubernetes-admin@${env_name}/g" $HOME/.kube/admin.conf
-sed -i "/${env_name}/d" ~/.zshrc_profile
+sed -i "/${env_name}/d" ~/.bash_profile
 
 sudo rm -f $HOME/.kube/${env_name}
 sudo mv $HOME/.kube/admin.conf $HOME/.kube/${env_name}
 
-echo 'alias '${env_name}'="export KUBECONFIG=$HOME/.kube/'${env_name}' && kubectl config use-context kubernetes-admin@'${env_name}'"' >> ~/.bash_profile
+echo 'alias '${env_name}'="export KUBECONFIG=$HOME/.kube/'${env_name}' && kubectl config use-context kubernetes-admin@'${env_name}'"' >> ~/.bash_profile 
