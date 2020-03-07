7cbef90672b7ced6b1d788bbfe92a237a716bc42
Florin <nenfl24@gmail.com>
2020-03-07 14:19:41 -0600

    added base remote state check

---
+++ remote_state.tf
@@ -0,0 +1,26 @@
+## AWS S3 Bucket ##
+
+data "terraform_remote_state" "base_config" {
+  backend = "s3"
+
+  config = {
+    bucket = "kube.omegnet.com"
+    key    = "${var.provider_name}/${var.deployment_environment}/${var.state_file_name}"
+    region = "${var.region}"
+  }
+}
+
+
+
+
+## Consul ##
+
+# data "terraform_remote_state" "base_config" {
+#   backend = "consul"
+
+#   config = {
+#     address = "consul.omegnet.com"
+#     scheme  = "http"
+#     path    = "${var.provider_name}/${var.deployment_environment}/${var.state_file_name}"
+#   }
+# }
