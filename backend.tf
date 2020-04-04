9541089ff4c29a810fc25b5bdf0145982b3961c0
Florin <nenfl24@gmail.com>
2020-04-03 20:14:11 -0500

    backend created

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "kube.omegnet.com"
-    key     = "vsphere/dev/terraform.tfstate"
+    key     = "vsphere/qa/qa-terraform.tfstate"
     region  = "eu-west-1"
   }
 }
