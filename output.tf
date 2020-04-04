1dd613f146845b23d9618a9cde909eb2ef3af38a
Florin <nenfl24@gmail.com>
2020-04-03 20:13:56 -0500

    get the name of vm's

--- output.tf
+++ output.tf
@@ -4,11 +4,11 @@ output "controller_ip" {
 output "node_ips" {
    value = "${vsphere_virtual_machine.kubernetes_nodes.*.default_ip_address}"
 }
-output "controller_vm-moref" {
-   value = "${vsphere_virtual_machine.kubernetes_controller.*.moid}"
+output "controller_vm-name" {
+   value = "${vsphere_virtual_machine.kubernetes_controller.*.name}"
 }
-output "node_vm-morefs" {
-   value = "${vsphere_virtual_machine.kubernetes_nodes.*.moid}"
+output "node_vm-name" {
+   value = "${vsphere_virtual_machine.kubernetes_nodes.*.name}"
 }
 output "kubeadm-init-info" {
    value = "${data.external.kubeadm-init-info.result}"
