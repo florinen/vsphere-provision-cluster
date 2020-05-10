0fae41b884290c585267968a729102f123e31c65
florinen <nenfl@yahoo.com>
2020-05-10 17:48:44 -0500

    added new rm line for bashrc

--- scripts/setup_cluster_access.sh
+++ scripts/setup_cluster_access.sh
@@ -1,9 +1,11 @@
+#/bin/bash -ex
 
 sed -i "/clusters/ {n;n;n;n;s/name: .*/name: ${env_name}/g}" $HOME/.kube/admin.conf
 sed -i "/contexts/ {n;n;s/cluster: .*/cluster: ${env_name}/g}" $HOME/.kube/admin.conf
 sed -i "/contexts/ {n;n;n;n;s/name: .*/name: kubernetes-admin@${env_name}/g}" $HOME/.kube/admin.conf
 sed -i "s/current-context: .*/current-context: kubernetes-admin@${env_name}/g" $HOME/.kube/admin.conf
 sed -i "/${env_name}/d" ~/.bash_profile
+sed -i "/${env_name}/d" ~/.bashrc
 
 sudo rm -f $HOME/.kube/${env_name}
 sudo mv $HOME/.kube/admin.conf $HOME/.kube/${env_name}
