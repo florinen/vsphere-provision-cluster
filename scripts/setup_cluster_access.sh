51a9158386736ba063930483e6bbfc6e42000e88
florinen <nenfl@yahoo.com>
2020-05-10 21:04:06 -0500

    some extras for later

--- scripts/setup_cluster_access.sh
+++ scripts/setup_cluster_access.sh
@@ -15,3 +15,13 @@ sudo mv $HOME/.kube/admin.conf $HOME/.kube/${env_name}
 echo "alias "${env_name}"='export KUBECONFIG=$HOME/.kube/"${env_name}" && kubectl config use-context kubernetes-admin@"${env_name}"'" >> ~/.zsh_profile
 
 
+
+
+
+# sed -i "/clusters/ {n;n;n;n;s/name: .*/name: ${env_name}/g}" $HOME/.kube/admin.conf
+# sed -i "/contexts/ {n;n;s/cluster: .*/cluster: ${env_name}/g}" $HOME/.kube/admin.conf
+# sed -i "/contexts/ {n;n;n;s/user: .*/user: admin@${env_name}/g}" $HOME/.kube/admin.conf
+# sed -i "/contexts/ {n;n;n;n;s/name: .*/name: admin@${env_name}/g}" $HOME/.kube/admin.conf
+# sed -i "s/current-context: .*/current-context: admin@${env_name}/g" $HOME/.kube/admin.conf
+# sed -i "/users/ {n;s/name: .*/name: admin/g}" $HOME/.kube/admin.conf
+
