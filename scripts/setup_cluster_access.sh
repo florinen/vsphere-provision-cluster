6170808ee2e440c684997e1ceeba78ec775f02b6
florinen <nenfl@yahoo.com>
2020-05-10 20:27:11 -0500

    fixed qutations

--- scripts/setup_cluster_access.sh
+++ scripts/setup_cluster_access.sh
@@ -6,8 +6,12 @@ sed -i "/contexts/ {n;n;n;n;s/name: .*/name: kubernetes-admin@${env_name}/g}" $H
 sed -i "s/current-context: .*/current-context: kubernetes-admin@${env_name}/g" $HOME/.kube/admin.conf
 sed -i "/${env_name}/d" ~/.bash_profile
 sed -i "/${env_name}/d" ~/.bashrc
+sed -i "/${env_name}/d" ~/.zshrc
+sed -i "/${env_name}/d" ~/.zsh_profile
 
 sudo rm -f $HOME/.kube/${env_name}
 sudo mv $HOME/.kube/admin.conf $HOME/.kube/${env_name}
 
-echo 'alias '${env_name}'="export KUBECONFIG=$HOME/.kube/'${env_name}' && kubectl config use-context kubernetes-admin@'${env_name}'"' >> ~/.bash_profile
+echo "alias "${env_name}"='export KUBECONFIG=$HOME/.kube/"${env_name}" && kubectl config use-context kubernetes-admin@"${env_name}"'" >> ~/.zsh_profile
+
+
